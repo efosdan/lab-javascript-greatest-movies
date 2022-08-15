e9e0668e6b66efe5d8597eb18d543e4a26324e40
efosdan <efosdan@yahoo.com>
2022-08-15 11:23:47 +0200

    First commit

--- src/movies.js
+++ src/movies.js
@@ -22,19 +22,46 @@ function howManyMovies(moviesArray) {
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
-
+function scoresAverage(moviesArray) {
+  movies.reduce((acc, val) => {
+    return acc + scoresAverage.val;
+  }, 0) / movies.length;
+}
+scoresAverage();
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  const dramaMovies = movies.filter((movie) => {
+    return movie.genre.include("Drama");
+  });
+  return dramaMoviesScore(dramaMovies);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  return movies.sort((a, b) => {
+    return a.year - b.year || a.title.localeCompare(b.title);
+  });
+}
+orderByYear(movies);
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+  return movies.map((movie) => {
+    const minutes = movie.duration.slice(
+      movie.duration.indexOf("") + 1,
+      movie.duration.indexOf("m")
+    );
+    const hoursInMinutes =
+      movie.duration.slice(0, movie.duration.indexOf("h")) * 60;
 
+    return { ...movie, duration: Number(minutes) + Number(hoursInMinutes) };
+  });
+}
+turnHoursToMinutes(movies);
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+  const years = [...new set(movies.map((movie) => movie.year))];
+}
